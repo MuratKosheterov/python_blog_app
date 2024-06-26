46de86e5a6138f2f9c5b1617f35a603be53477c2
Murat <kmx31525@gmail.com>
2024-06-26 18:37:13 +0500

    first commit

---
+++ posts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
+
+# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'base.html'
+
+
+
